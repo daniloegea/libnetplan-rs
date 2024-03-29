d45c07e1ab63fa718315be0747a9a20ff946bbf6
Danilo Egea Gondolfo <danilogondolfo@gmail.com>
2024-03-29 16:47:29 +0000

    state: implement some unit tests

--- libnetplan/src/state.rs
+++ libnetplan/src/state.rs
@@ -28,7 +28,7 @@ use crate::libnetplan::NetplanState;
 use crate::parser::Parser;
 
 pub struct State {
-    state: *mut NetplanState,
+    pub(crate) state: *mut NetplanState,
     iter: *mut netdef_pertype_iter,
 }
 
@@ -143,5 +143,162 @@ impl Iterator for State {
 impl Drop for State {
     fn drop(&mut self) {
         unsafe { netplan_state_clear(&mut self.state) };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::{self, File};
+    use std::io::Write;
+    use std::os::unix::fs::PermissionsExt;
+    use tempfile::tempdir;
+
+    fn create_parser(yaml: &str) -> Parser {
+        let root_dir = tempdir().expect("Cannot create tempdir for test");
+        let filename = root_dir.path().join("10-config.yaml");
+
+        let mut tmp_file = File::create(filename).expect("Cannot create tempfile for test");
+        let mut perms = tmp_file.metadata().unwrap().permissions();
+        perms.set_mode(0o0600);
+        tmp_file
+            .set_permissions(perms)
+            .expect("Cannot set permission to 600");
+
+        let yaml = yaml.as_bytes();
+
+        tmp_file
+            .write(yaml)
+            .expect("Cannot write YAML content for test");
+
+        let parser = Parser::new();
+
+        let filename_str = root_dir
+            .path()
+            .join("10-config.yaml")
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        let parser_result = parser.load_yaml(&filename_str);
+
+        assert!(parser_result.is_ok());
+
+        fs::remove_file(root_dir.path().join("10-config.yaml")).expect("Cannot remove file");
+        root_dir.close().expect("Cannot close directory");
+
+        parser
+    }
+
+    #[test]
+    fn test_create_a_state() {
+        let state = State::new();
+        assert!(state.state != ::std::ptr::null_mut());
+    }
+
+    #[test]
+    fn test_import_parser_results_validation_error() {
+        let yaml = r"
+network:
+  vrfs:
+    vrf0:
+      table: 1000
+      routes:
+        - to: 192.168.0.0/24
+          via: 1.2.3.4
+          table: 2000
+  ethernets:
+    eth0:
+      dhcp4: true
+    eth1: {}
+    eth2:
+      addresses:
+        - 192.168.0.1/24
+        - 10.20.30.40/24
+      nameservers:
+        addresses:
+          - 192.168.0.254
+        search:
+          - mydomain.local";
+
+        let parser = create_parser(yaml);
+
+        let state = State::new();
+
+        if let Err(error) = state.import_parser_state(parser) {
+            if let LibNetplanError::NetplanValidationError(msg) = error {
+                assert_eq!(msg, "vrf0: VRF routes table mismatch (1000 != 2000)");
+            }
+        } else {
+            assert!(false, "This test should have passed but didn't");
+        }
+    }
+
+    #[test]
+    fn test_import_parser_results() {
+        let yaml = r"
+network:
+  ethernets:
+    eth0:
+      dhcp4: true
+    eth1: {}
+    eth2:
+      addresses:
+        - 192.168.0.1/24
+        - 10.20.30.40/24
+      routes:
+        - to: default
+          via: 192.168.0.254
+      nameservers:
+        addresses:
+          - 192.168.0.254
+        search:
+          - mydomain.local";
+
+        let parser = create_parser(yaml);
+
+        let state = State::new();
+
+        if let Err(_) = state.import_parser_state(parser) {
+            assert!(false, "load parser results failed");
+        }
+    }
+
+    #[test]
+    fn test_state_iterator() {
+        let yaml = r"
+network:
+  ethernets:
+    eth0:
+      dhcp4: true
+    eth1: {}
+    eth2:
+      addresses:
+        - 192.168.0.1/24
+        - 10.20.30.40/24
+      routes:
+        - to: default
+          via: 192.168.0.254
+      nameservers:
+        addresses:
+          - 192.168.0.254
+        search:
+          - mydomain.local";
+
+        let parser = create_parser(yaml);
+
+        let state = State::new();
+        state.import_parser_state(parser).unwrap();
+
+        let mut netdef_ids = Vec::new();
+        let netdef_ids_expected = vec!["eth0", "eth1", "eth2"];
+
+        for netdef in state {
+            netdef_ids.push(netdef.name.clone());
+        }
+
+        for expected in netdef_ids_expected {
+            assert!(netdef_ids.contains(&expected.to_string()));
+        }
     }
 }
